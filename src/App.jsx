4d302c2dc16bad991ea5474b017b9dabdcabcf65
MohidulCreative <90836840+MhidulCreative@users.noreply.github.com>
2024-01-25 23:18:29 +0530

    packages installed and created some folder and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
     <>
-     <h2>Hello world Programme!</h2>
+     <h2 className='p-3 bg-orange-600 '>Hello world Programme! let 's write another text</h2>
+
+     <div className='bg-orange-600 p-6 m-2 mx-auto max-w-80'>
+        <h2>Product A agent property</h2>
+        <span>price: 200</span>
+        <p>daily get: 95</p>
+        <p>total income: 1800</p>
+     </div>
+     <div className='bg-orange-600 p-6 m-2 mx-auto max-w-80'>
+        <h2>Product A agent property</h2>
+        <span>price: 200</span>
+        <p>daily get: 95</p>
+        <p>total income: 1800</p>
+     </div>
+     <div className='bg-orange-600 p-6 m-2 mx-auto max-w-80'>
+        <h2>Product A agent property</h2>
+        <span>price: 200</span>
+        <p>daily get: 95</p>
+        <p>total income: 1800</p>
+     </div>
+     <div className='bg-orange-600 p-6 m-2 mx-auto max-w-80'>
+        <h2>Product A agent property</h2>
+        <span>price: 200</span>
+        <p>daily get: 95</p>
+        <p>total income: 1800</p>
+     </div>
+     <div className='bg-orange-600 p-6 m-2 mx-auto max-w-80'>
+        <h2>Product A agent property</h2>
+        <span>price: 200</span>
+        <p>daily get: 95</p>
+        <p>total income: 1800</p>
+     </div>
+
+     <Footer />
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <nav className="navbar">
+        <a href="contact.html"> <i className="fas fa-address-book"></i> <span>about</span> </a>
+        <a href="portfolio.html"> <i className="fas fa-blog"></i> <span>care</span> </a>
+        <a href="blogs.html"> <i className="fas fa-briefcase"></i> <span>plan</span> </a>
+        <a href="about.html"> <i className="fas fa-user"></i> <span>me</span> </a>
+    </nav>
+    </>
+  )
+}
+
+export default Footer
