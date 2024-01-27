87dbe2eb7c2e395ce521e9100d6b57d06d9e8ee7
MohidulCreative <90836840+MhidulCreative@users.noreply.github.com>
2024-01-27 22:29:37 +0530

    change on options

--- src/components/Home/QuckMenu.jsx
+++ src/components/Home/QuckMenu.jsx
@@ -3,18 +3,18 @@ import React from "react";
 
 const QuckMenu = () => {
     return (
-        <div className="flex gap-3 p-2 mb-4 justify-center items-center">
+        <div className="flex gap-4 p-2 pt-0 mb-4 justify-center items-center">
             {/* Home */}
-            <div className="text-center">
-                <div className="rounded-full p-3 bg-black bg-opacity-90 text-white inline-block">
+            <div className="text-center flex flex-col justify-center items-center">
+                <div className="rounded-full w-4 h-4 p-3 bg-orange-700 bg-opacity-90 flex justify-center items-center text-white">
                     <i className="fas fa-home text-blue-700 text-sm"></i>
                 </div>
                 <p className="text-gray-300 text-xs mt-1">Home</p>
             </div>
 
             {/* Telegram */}
-            <div className="text-center">
-                <div className="rounded-full p-3 bg-black bg-opacity-30 text-white inline-block">
+            <div className="text-center flex flex-col justify-center items-center">
+                <div className="rounded-full w-4 h-4 flex justify-center items-center p-3 bg-black bg-opacity-30 text-white">
                     <i className="fas fa-paper-plane text-teal-500"></i>
                 </div>
                 <p className="text-gray-300 text-xs mt-1 font-semibold">
@@ -23,8 +23,8 @@ const QuckMenu = () => {
             </div>
 
             {/* WhatsApp */}
-            <div className="text-center">
-                <div className="rounded-full p-3 bg-black bg-opacity-30 text-white inline-block">
+            <div className="text-center flex flex-col justify-center items-center">
+                <div className="rounded-full w-4 h-4 flex justify-center items-center p-3 bg-black bg-opacity-30 text-white">
                     <i className="fab fa-whatsapp text-red-500"></i>
                 </div>
                 <p className="text-gray-300 text-xs mt-1 font-semibold">
@@ -33,8 +33,8 @@ const QuckMenu = () => {
             </div>
 
             {/* Service */}
-            <div className="text-center">
-                <div className="rounded-full p-3 bg-black bg-opacity-30 text-white inline-block">
+            <div className="text-center flex flex-col justify-center items-center">
+                <div className="rounded-full w-4 h-4 flex justify-center items-center p-3 bg-black bg-opacity-30 text-white">
                     <i className="fas fa-cogs text-orange-500"></i>
                 </div>
                 <p className="text-gray-300 text-xs mt-1 font-semibold">
@@ -43,8 +43,8 @@ const QuckMenu = () => {
             </div>
 
             {/* Team */}
-            <div className="text-center">
-                <div className="rounded-full p-3 bg-black bg-opacity-30 text-white inline-block">
+            <div className="text-center flex flex-col justify-center items-center">
+                <div className="rounded-full w-4 h-4 flex justify-center items-center p-3 bg-black bg-opacity-30 text-white">
                     <i className="fas fa-users text-purple-500"></i>
                 </div>
                 <p className="text-gray-300 text-xs mt-1 font-semibold">Team</p>
