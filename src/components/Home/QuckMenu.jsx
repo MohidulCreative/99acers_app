c301660a1eadae515e677c7a5e00429b2e972567
MohidulCreative <90836840+MhidulCreative@users.noreply.github.com>
2024-01-27 21:42:52 +0530

    fixed the slider and iptions menu

--- src/components/Home/QuckMenu.jsx
+++ src/components/Home/QuckMenu.jsx
@@ -2,49 +2,53 @@
 import React from "react";
 
 const QuckMenu = () => {
-  return (
-    <div className="flex gap-3 p-2 justify-center items-center">
-         {/* Home */}
-         <div className="text-center">
-          <div className="rounded-full p-2 bg-blue-500 text-white inline-block">
-            <i className="fas fa-home text-xl"></i>
-          </div>
-          <p className="text-xl font-semibold mb-2 text-white">Home</p>
-        </div>
+    return (
+        <div className="flex gap-4 p-2 justify-center items-center">
+            {/* Home */}
+            <div className="text-center">
+                <div className="rounded-full p-3 bg-gray-300 text-white inline-block">
+                    <i className="fas fa-home text-blue-700 text-sm"></i>
+                </div>
+                <p className="text-white text-sm">Home</p>
+            </div>
 
-        {/* Telegram */}
-        <div className="text-center">
-          <div className="rounded-full p-2 bg-teal-500 text-white inline-block">
-            <i className="fas fa-paper-plane text-xl"></i>
-          </div>
-          <p className="text-xl font-semibold mb-2 text-white">Telegram</p>
-        </div>
+            {/* Telegram */}
+            <div className="text-center">
+                <div className="rounded-full p-3 bg-gray-300 text-white inline-block">
+                    <i className="fas fa-paper-plane text-teal-500"></i>
+                </div>
+                <p className="text-white text-sm font-semibold mb-2">
+                    Telegram
+                </p>
+            </div>
 
-        {/* WhatsApp */}
-        <div className="text-center">
-          <div className="rounded-full p-2 bg-green-500 text-white inline-block">
-            <i className="fab fa-whatsapp text-xl"></i>
-          </div>
-          <p className="text-xl font-semibold mb-2 text-white">WhatsApp</p>
-        </div>
+            {/* WhatsApp */}
+            <div className="text-center">
+                <div className="rounded-full p-3 bg-gray-300 text-white inline-block">
+                    <i className="fab fa-whatsapp text-red-500"></i>
+                </div>
+                <p className="text-white text-sm font-semibold mb-2">
+                    WhatsApp
+                </p>
+            </div>
 
-        {/* Service */}
-        <div className="text-center">
-          <div className="rounded-full p-2 bg-orange-500 text-white inline-block">
-            <i className="fas fa-cogs text-xl"></i>
-          </div>
-          <p className="text-xl font-semibold mb-2 text-white">Service</p>
-        </div>
+            {/* Service */}
+            <div className="text-center">
+                <div className="rounded-full p-3 bg-gray-300 text-white inline-block">
+                    <i className="fas fa-cogs text-orange-500"></i>
+                </div>
+                <p className="text-white text-sm font-semibold mb-2">Service</p>
+            </div>
 
-        {/* Team */}
-        <div className="text-center">
-          <div className="rounded-full p-2 bg-purple-500 text-white inline-block">
-            <i className="fas fa-users text-xl"></i>
-          </div>
-          <p className="text-xl font-semibold mb-2 text-white">Team</p>
+            {/* Team */}
+            <div className="text-center">
+                <div className="rounded-full p-3 bg-gray-300 text-white inline-block">
+                    <i className="fas fa-users text-purple-500"></i>
+                </div>
+                <p className="text-white text-sm font-semibold mb-2">Team</p>
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default QuckMenu;
