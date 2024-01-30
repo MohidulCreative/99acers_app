e3b1a0db87fe4ffedfa8ed62951d2e7f5eea1045
MohidulCreative <90836840+MhidulCreative@users.noreply.github.com>
2024-01-31 00:26:34 +0530

    logo issue

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
         <div className="w-full fixed left-0 top-0 z-50 bg-black flex justify-between items-center mx-auto px-8 py-2 text-white">
-            <Link className='text-xl' to="/"><img className=' h-12' src="/logo2.png" alt="" /></Link>
+            <Link className='text-xl' to="/"><img className=' h-12' src="/logo.png" alt="" /></Link>
             <div className='bg-[crimson] p-2 flex justify-center items-center rounded-full'>
                 <Link to="/profile" className='text-xs font-semibold text-white'>VIP-0</Link>
             </div>
