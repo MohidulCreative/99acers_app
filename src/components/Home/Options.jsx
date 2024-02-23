02ef084dba2ee1045dab5d4ef227369fa2aaf526
MohidulCreative <90836840+MhidulCreative@users.noreply.github.com>
2024-02-23 22:35:20 +0530

    some link changes in options .jsx component

--- src/components/Home/Options.jsx
+++ src/components/Home/Options.jsx
@@ -1,58 +1,71 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Options = () => {
     return (
         <div className="grid grid-cols-3 gap-y-8 py-8 px-2">
             <div className="text-center flex flex-col justify-center items-center">
-                <div className="rounded-xl w-10 h-10 p-8 flex justify-center items-center bg-gray-500 bg-opacity-30">
-                    <span className="material-symbols-outlined text-blue-700 text-4xl">
-                        checkbook
-                    </span>
-                </div>
-                <p className="text-gray-300 text-sm mt-2">Recharge</p>
+                <Link to="deposit">
+                    <div className="rounded-xl w-10 h-10 p-8 flex justify-center items-center bg-gray-500 bg-opacity-30">
+                        <span className="material-symbols-outlined text-blue-700 text-4xl">
+                            checkbook
+                        </span>
+                    </div>
+                    <p className="text-gray-300 text-sm mt-2">Recharge</p>
+                </Link>
             </div>
 
             <div className="text-center flex flex-col justify-center items-center">
-                <div className="rounded-xl w-10 h-10 p-8 flex justify-center items-center bg-gray-500 bg-opacity-30">
-                    <span className="material-symbols-outlined text-blue-700 text-4xl">
-                        open_in_browser
-                    </span>
-                </div>
-                <p className="text-gray-300 text-sm mt-2">Widthdraw</p>
+                <Link to="widthdraw">
+                    <div className="rounded-xl w-10 h-10 p-8 flex justify-center items-center bg-gray-500 bg-opacity-30">
+                        <span className="material-symbols-outlined text-blue-700 text-4xl">
+                            open_in_browser
+                        </span>
+                    </div>
+                    <p className="text-gray-300 text-sm mt-2">Widthdraw</p>
+                </Link>
             </div>
 
             <div className="text-center flex flex-col justify-center items-center">
-                <div className="rounded-xl w-10 h-10 p-8 flex justify-center items-center bg-gray-500 bg-opacity-30">
-                    <span className="material-symbols-outlined text-blue-700 text-4xl">
-                        groups
-                    </span>
-                </div>
-                <p className="text-gray-300 text-sm mt-2">Team</p>
+                <Link to="teams">
+                    <div className="rounded-xl w-10 h-10 p-8 flex justify-center items-center bg-gray-500 bg-opacity-30">
+                        <span className="material-symbols-outlined text-blue-700 text-4xl">
+                            groups
+                        </span>
+                    </div>
+                    <p className="text-gray-300 text-sm mt-2">Team</p>
+                </Link>
             </div>
 
             <div className="text-center flex flex-col justify-center items-center">
-                <div className="rounded-xl w-10 h-10 p-8 flex justify-center items-center bg-gray-500 bg-opacity-30">
-                    <span className="material-symbols-outlined text-blue-700 text-4xl">
-                        hub
-                    </span>
-                </div>
-                <p className="text-gray-300 text-sm mt-2">How works</p>
+                <Link to="how_works">
+                    <div className="rounded-xl w-10 h-10 p-8 flex justify-center items-center bg-gray-500 bg-opacity-30">
+                        <span className="material-symbols-outlined text-blue-700 text-4xl">
+                            hub
+                        </span>
+                    </div>
+                    <p className="text-gray-300 text-sm mt-2">How works</p>
+                </Link>
             </div>
             <div className="text-center flex flex-col justify-center items-center">
-                <div className="rounded-xl w-10 h-10 p-8 flex justify-center items-center bg-gray-500 bg-opacity-30">
-                    <span className="material-symbols-outlined text-blue-700 text-4xl">
-                        open_in_new
-                    </span>
-                </div>
-                <p className="text-gray-300 text-sm mt-2">My project</p>
+                <Link to="my_project">
+                    <div className="rounded-xl w-10 h-10 p-8 flex justify-center items-center bg-gray-500 bg-opacity-30">
+                        <span className="material-symbols-outlined text-blue-700 text-4xl">
+                            open_in_new
+                        </span>
+                    </div>
+                    <p className="text-gray-300 text-sm mt-2">My project</p>
+                </Link>
             </div>
             <div className="text-center flex flex-col justify-center items-center">
-                <div className="rounded-xl w-10 h-10 p-8 flex justify-center items-center bg-gray-500 bg-opacity-30">
-                    <span className="material-symbols-outlined text-blue-700 text-4xl">
-                      browse_activity
-                    </span>
-                </div>
-                <p className="text-gray-300 text-sm mt-2">Activity</p>
+                <Link to="activity">
+                    <div className="rounded-xl w-10 h-10 p-8 flex justify-center items-center bg-gray-500 bg-opacity-30">
+                        <span className="material-symbols-outlined text-blue-700 text-4xl">
+                            browse_activity
+                        </span>
+                    </div>
+                    <p className="text-gray-300 text-sm mt-2">Activity</p>
+                </Link>
             </div>
         </div>
     );
